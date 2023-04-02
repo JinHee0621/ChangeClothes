566aba066945cecef73b23ec1bd4867d002f7369
JinHee9406 <lsd0017@naver.com>
2023-04-02 21:07:34 +0900

    Add ClothSet and Etc..
    2023-04-02
    ---------------------
    - 옷 세트의 이미지 및 사운드 추가
    - 바지 세트 추가
    - gitignore 수정

--- ChangeClothes/Assets/Scripts/ClothSetManager.cs
+++ ChangeClothes/Assets/Scripts/ClothSetManager.cs
@@ -7,17 +7,22 @@ public class ClothSetManager : MonoBehaviour
     public GameObject shirtSet;
     public GameObject pantsSet;
     GameObject[] hangerSet;
+    GameObject[] pantsHangerSet;
+
 
     private bool isShirtSetOpen = false;
+    private bool isPantsSetOpen = false;
 
     private void Start()
     {
         hangerSet = GameObject.FindGameObjectsWithTag("Hanger");
+        pantsHangerSet = GameObject.FindGameObjectsWithTag("Hanger_Pants");
     }
 
     public void ShirtSetOpen()
     {
-        if(!isShirtSetOpen)
+        SoundManager.PlaySFX(4);
+        if (!isShirtSetOpen)
         {
             shirtSet.GetComponent<Animator>().SetBool("Open", true);
             foreach(GameObject ele in hangerSet)
@@ -36,4 +41,26 @@ public class ClothSetManager : MonoBehaviour
         }
     }
 
+    public void PantsSetOpen()
+    {
+        SoundManager.PlaySFX(4);
+        if (!isPantsSetOpen)
+        {
+            pantsSet.GetComponent<Animator>().SetBool("Open", true);
+            foreach (GameObject ele in pantsHangerSet)
+            {
+                ele.GetComponent<Animator>().SetTrigger("GetClothSet");
+            }
+            isPantsSetOpen = true;
+        }
+        else
+        {
+            pantsSet.GetComponent<Animator>().SetBool("Open", false);
+            foreach (GameObject ele in pantsHangerSet)
+            {
+                ele.GetComponent<Animator>().SetTrigger("GetClothSet");
+            }
+            isPantsSetOpen = false;
+        }
+    }
 }
